1a90340bca47850b81dc0ad470753809c14aa933
Juliemakary <124319413+Juliemakary@users.noreply.github.com>
2023-10-24 12:32:54 -0400

    create.js
    create operation on properties

--- soen341.js
+++ soen341.js
@@ -5,21 +5,15 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended: ture}));
 
-
-app.get('/create-property', (req, res) => {
-    // Render a form for users to input property details
-    res.send('<form method="POST" action="/create-property">' +
-      '<input type="text" name="title" placeholder="Title"><br>' +
-      '<input type="text" name="description" placeholder="Description"><br>' +
-      '<input type="number" name="price" placeholder="Price"><br>' +
-      '<button type="submit">Create Property</button>' +
-      '</form>'
-    );
-  });
-
 app.post('/create-property', async(req, res)); {
+ try{
 const {title, description, price}=req.body;
-const item = new Item({title, description, price});
-properties.push(property);
+const property = new property({title, description, price});
+await property.save();
+res.status(201).json(property);
 res.send('property created successfully');
+
+ } catch(error){
+  res.status(500).json({error: 'Could not create property'});
+ }
 }
