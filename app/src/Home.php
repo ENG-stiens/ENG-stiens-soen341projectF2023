48d46e4eb6ad4a8f5af0bd5d14f9f5d6d3ccc98d
SarOhayon <135247936+SarOhayon@users.noreply.github.com>
2023-10-25 13:52:54 -0400

    made home php file

--- app/src/Home.php
+++ app/src/Home.php
@@ -5,7 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>UniversalHomepage</title>
         <link rel="stylesheet" href="styles.css">
-       
     </head>
     <body>
         <header>
@@ -15,51 +14,39 @@
                <h1>
                     <img src="house.png" alt="Logo">
                     Eng-Stiens' Real Estate Aid
-                    
                </h1>
-
             
                 <nav>
                     <ul>
                         <li>
-                         <a href="#">Homebuyer</a>
-                         <div class="dropdown">
-                                <a href="#">Search Houses</a>
-                                <a href="#">Search Brokers</a>
-                                <a href="#">Morgage Calulator</a>
-                            </div>
-                        </li>
-                      <li><a href="#">Renter</a>
+                            <a href="#">Homebuyer</a>
                             <div class="dropdown">
                                 <a href="#">Search Houses</a>
                                 <a href="#">Search Brokers</a>
-                            </div></li>
-                     <li><a href="#">Broker</a>
-                        <div class="dropdown">
-                            <a href="#">Search Houses filtered by broker</a>
-                        </div></li>
-                    <li><a href="#">Contact Us</a></li>
-                </ul>
-             </nav>
+                                <a href="#">Mortgage Calculator</a>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#">Renter</a>
+                            <div class="dropdown">
+                                <a href="#">Search Houses</a>
+                                <a href="#">Search Brokers</a>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#">Broker</a>
+                            <div class="dropdown">
+                                <a href="#">Search Houses filtered by broker</a>
+                            </div>
+                        </li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </nav>
             </div>
-        
         </header>
 
-        <section id="featured-properties">
-            <!-- ... content ... -->
-            
-        </section>
-
-        <section id="about-us">
-            <!-- ... content ... -->
-        </section>
-
-        <section id="contact">
-            <!-- ... content ... -->
-        </section>
-
         <footer>
-            <p>&copy; 2023 Your Real Estate Company</p>
+            <p>&copy; <?php echo date("Y"); ?> Your Real Estate Company</p>
         </footer>
     </body>
 </html>
