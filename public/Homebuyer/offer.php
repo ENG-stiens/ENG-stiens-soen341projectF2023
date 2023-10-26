b285ba5320b37dfa89e6ba87ebf14faa4d3ada02
SarOhayon <135247936+SarOhayon@users.noreply.github.com>
2023-10-26 14:53:22 -0400

    added offer page for homebuyer

--- public/Homebuyer/offer.php
+++ public/Homebuyer/offer.php
@@ -1,22 +1,40 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
-    <link rel="stylesheet" href="../styles.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400&family=Russo+One&display=swap" rel="stylesheet">
-    <meta charset="UTF-8">
-    <title>Offer</title>
+    <title>Submit Your Offer</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <?php
 include 'header.php';
 include 'sidenav.php';
 ?>
 <body>
-    <div class="content">
-        <h1>content area</h1> <!-- add stuff here -->
+
+    <div class="container">
+        <h1>Submit Your Offer</h1>
+        <p>Please fill out the form below to submit your offer for the desired property.</p>
+
+        <form action="/submit_offer" method="post">
+            <label for="name">Name:</label><br>
+            <input type="text" id="name" name="name" required><br><br>
+
+            <label for="email">Email:</label><br>
+            <input type="email" id="email" name="email" required><br><br>
+
+            <label for="phone">Phone Number:</label><br>
+            <input type="text" id="phone" name="phone" required><br><br>
+
+            <label for="address">Property Address:</label><br>
+            <input type="text" id="address" name="address" required><br><br>
+
+            <label for="offer">Your Offer ($):</label><br>
+            <input type="number" id="offer" name="offer" required><br><br>
+
+            <input type="submit" value="Submit Offer">
+        </form>
     </div>
-    <?php
+<?php
 include 'footer.php';
 ?>
 </body>
