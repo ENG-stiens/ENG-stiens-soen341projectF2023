bc539c8e748aac735976f0a6139062bc51b4ca92
SarOhayon <135247936+SarOhayon@users.noreply.github.com>
2023-10-26 15:25:30 -0400

    fixed nav bar

--- public/Homebuyer/sidenav.php
+++ public/Homebuyer/sidenav.php
@@ -2,37 +2,38 @@
 <html>
     <style>
 .content {
-  flex: 1;
-  padding: 20px;
-}
+            flex: 1;
+            padding: 20px;
+            margin-left: 250px; /* Set the same width as the sidenav to create space */
+        }
 
-.wrapper {
-  display: flex;
-}
+        .wrapper {
+            display: flex;
+        }
 
-.sidenav {
-  width: 250px; /* Adjusted the width to match the margin-left in the .content class */
-  height: 100vh;
-  position: fixed; /* Changed to fixed for a sticky sidebar */
-  z-index: 1;
-  top: 0;
-  left: 0;
-  overflow-x: hidden;
-  padding-top: 20px;
-  background: #2E2C2F;
-}
+        .sidenav {
+            width: 250px;
+            height: 100vh;
+            position: fixed;
+            z-index: 1;
+            top: 0;
+            left: 0;
+            overflow-x: hidden;
+            padding-top: 20px;
+            background: #2E2C2F;
+        }
 
-.sidenav a {
-  padding: 16px;
-  text-decoration: none;
-  font-size: 20px;
-  display: block;
-  color: #EAEAEA;
-}
+        .sidenav a {
+            padding: 16px;
+            text-decoration: none;
+            font-size: 20px;
+            display: block;
+            color: #EAEAEA;
+        }
 
-.sidenav a:hover {
-  color: #BACDB0;
-
+        .sidenav a:hover {
+            color: #BACDB0;
+        }
         </style>
 <body>
 
