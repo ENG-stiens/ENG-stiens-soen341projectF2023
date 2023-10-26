7ec3c2de4ab11664a61727f3686b976348286bca
SarOhayon <135247936+SarOhayon@users.noreply.github.com>
2023-10-25 20:09:24 -0400

    fixed login button

--- app/src/headerfooter.php
+++ app/src/headerfooter.php
@@ -9,16 +9,12 @@
     </head>
     <body>
         <header>
-            <button class="login-button">Log in</button>
-
             <div class="title-container">
-               <h1>
+               <h1><button onclick="window.location.href='login.php'">Log in</button>
                     <img src="house.png" alt="Logo">
                     Eng-Stiens' Real Estate Aid
                     
                </h1>
-
-            
                 <nav>
                     <ul>
                         <li>
@@ -42,22 +38,6 @@
                 </ul>
              </nav>
             </div>
-            <?php
-    if (session_status() == PHP_SESSION_NONE) {
-        session_start();
-    }
-    if (isset($_SESSION["logged_in"]) && $_SESSION["logged_in"] === true) {
-        echo '<div class="dropdown">';
-        echo '<img src="user_icon.png" alt="User Icon">';
-        echo '<div class="dropdown-content">';
-        echo '<a href="logout.php">Logout</a>';
-        echo '</div>';
-        echo '</div>';
-    } else {
-        echo '<a href="login.php">Login</a>';
-        echo '<a href="createAccount.php">Create an Account</a>';
-    }
-    ?>
         </header>
 
         <footer>
