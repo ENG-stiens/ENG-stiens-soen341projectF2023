e850b029f8f2df373b258cf6bc6b1faf2d08c045
SarOhayon <135247936+SarOhayon@users.noreply.github.com>
2023-10-26 15:33:50 -0400

    fixing side nav

--- public/Homebuyer/listings.php
+++ public/Homebuyer/listings.php
@@ -47,38 +47,7 @@ h1 {
   border-radius: 5px;
 }
 
-/* .content {
-  flex: 1;
-  padding: 20px;
-}
 
-.wrapper {
-  display: flex;
-}
-
-.sidenav {
-  width: 250px; /* Adjusted the width to match the margin-left in the .content class */
-  height: 100vh;
-  position: fixed; /* Changed to fixed for a sticky sidebar */
-  z-index: 1;
-  top: 0;
-  left: 0;
-  overflow-x: hidden;
-  padding-top: 20px;
-  background: #2E2C2F;
-}
-
-.sidenav a {
-  padding: 16px;
-  text-decoration: none;
-  font-size: 20px;
-  display: block;
-  color: #EAEAEA;
-}
-
-.sidenav a:hover {
-  color: #BACDB0;
-} */
 
     </style>
   </head>
