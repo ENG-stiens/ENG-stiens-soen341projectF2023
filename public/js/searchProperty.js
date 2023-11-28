25f57bc65c42074017c7da86165f05a42a3b3e6b
eo2000 <olieleden0@gmail.com>
2023-11-28 17:53:33 -0500

    fixes for offer management and appointment management

--- public/js/searchProperty.js
+++ public/js/searchProperty.js
@@ -1,74 +1,58 @@
-
 // function to display property information
 function displayPropertyInfo(propertyData) {
     const propertyInfoContainer = document.querySelector('.propertyInfo'); // uses the class declared above
     propertyInfoContainer.innerHTML = '';
 
-// wrappers are used for styling the display of the content
+    // wrappers are used for styling the display of the content
     const wrapper = document.createElement('div');
     wrapper.classList.add('labelsInfo');
 
-// display info
-    const propertyAddress = createInfoElement('Name:', propertyData.address);
-    const propertyAgency = createInfoElement('Name:', propertyData.agency);
-    const propertyPhone = createInfoElement('Name:', propertyData.phone);
-    const propertyDescription = createInfoElement('Name:', propertyData.description);
-    const propertyPrice = createInfoElement('Name:', propertyData.price);
-    const propertyAmeneties = createInfoElement('Name:', propertyData.ameneties);
+    // display info
+    const propertyAddress = createInfoElement('Address:', propertyData.address);
+    const propertyAgency = createInfoElement('Agency:', propertyData.agency);
+    const propertyPhone = createInfoElement('Phone:', propertyData.phone);
+    const propertyDescription = createInfoElement('Description:', propertyData.description);
+    const propertyPrice = createInfoElement('Price:', propertyData.price);
+    const propertyAmenities = createInfoElement('Amenities:', propertyData.amenities); // Corrected typo
 
-
-// send info to website page
+    // send info to website page
     wrapper.appendChild(propertyAddress);
     wrapper.appendChild(propertyAgency);
     wrapper.appendChild(propertyPhone);
     wrapper.appendChild(propertyDescription);
     wrapper.appendChild(propertyPrice);
-    wrapper.appendChild(propertyAmeneties);
-    brokerInfoContainer.appendChild(wrapper);
+    wrapper.appendChild(propertyAmenities); // Corrected variable name
+    propertyInfoContainer.appendChild(wrapper); // Corrected variable name
+}
 
-
-    const InfoWrapper = document.createElement('div');
-    InfoWrapper.classList.add('labelsInfo');
-
-
-
-// function to create a box to diplay the information of property
-// less repetitve code
+// function to create a box to display the information of property
+// less repetitive code
 function createInfoElement(label, value) {
     const infoElement = document.createElement('div');
     infoElement.innerHTML = `
-    <p>${label}</p>
-    <div class="infoBox">${value}</div>
+        <p>${label}</p>
+        <div class="infoBox">${value}</div>
     `;
-// returns filled out information
+    // returns filled out information
     return infoElement;
 }
 
-
-//form submission
+// form submission
 // test data
 function searchProperty() {
-// test
+    // test
     const mockPropertyData = {
         address: '10 Greendale',
         agency: 'ABC Realty',
         phone: '123-456-7890',
         description: 'Beautiful Single Family Home',
         price: '$500,000',
-        ameneties: 'Spacious backyard, modern kitchen, garage',
-
+        amenities: 'Spacious backyard, modern kitchen, garage', // Corrected variable name
     };
 
-// display data
-    displayPropertyInfo(mockPropertyDataData);
+    // display data
+    displayPropertyInfo(mockPropertyData);
 }
 
-
-// searchBrokers function
-// this will show asha paris by default
-// this needs to change
-const searchForm = document.querySelector('.searchBar_properties');
-searchForm.addEventListener('submit', event => {
-    event.preventDefault();
-    searchProperty();
-});}
+// Calling the searchProperty function to test
+searchProperty();
